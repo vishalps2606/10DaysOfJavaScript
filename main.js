273ce265b603cdc64d5a3a4428bbb38fc826ccba
Vishal pandey <70359874+vishalps2606@users.noreply.github.com>
2021-02-04 21:05:09 +0530

    Added clock

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
 
-const secondHand = document.querySelector('.second-hand');
-const minHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
 
-function setDate(){
-    const now = new Date();
+const deg = 6;
 
-    const seconds = now.getSeconds();
-    const secondDegree = ((seconds/60)*360)+90;
-    console.log(secondDegree);
-    secondHand.style.transform = `rotate(${secondDegree}deg)`;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
 
-    const min = now.getMinutes();
-    const minDegree = ((min/60)*360)+90;
-    minHand.style.transform = `rotate(${minDegree}deg)`;
+setInterval(() => {
 
-    const hour = now.getHours();
-    console.log(hour);
-    const hourDegree = ((hour/12)*360)+90;
-    hourHand.style.transform = `rotate(${hourDegree}deg)`;
-}
-setInterval(setDate,1000);
+    let day = new Date();
+    let hh = day.getHours()*30;
+    let mm = day.getMinutes()*deg;
+    let ss = day.getSeconds()*deg;
+
+    hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+})
